2b24cfe236e0e2bb88c7f0c6fc2afd11fa735fe6
Eric Klinginsmith <eric.klinginsmith@email.wsu.edu>
2014-03-06 19:02:39 -0800

    Set up XCode project

---
+++ mobileDrive/MobileDriveAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MobileDriveAppDelegate.h
+//  mobileDrive
+//
+//  Created by Eric Klinginsmith on 3/6/14.
+//  Copyright (c) 2014 Eric Klinginsmith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MobileDriveAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ mobileDrive/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  mobileDrive
+//
+//  Created by Eric Klinginsmith on 3/6/14.
+//  Copyright (c) 2014 Eric Klinginsmith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MobileDriveAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MobileDriveAppDelegate class]));
+    }
+}
