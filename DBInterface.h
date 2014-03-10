2b316acdb361177d078509f33435ed0d7c6564c1
jesse.pitel <jessepcyt89@gmail.com>
2014-03-10 16:21:07 -0700

    Finished DB wrapper.

--- DBInterface.h
+++ DBInterface.h
@@ -7,7 +7,17 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "mobileDrive/dbfs/dbfs.h"
 
 @interface DBInterface : NSObject
+
+-(char *)nsStringToCString:(NSString *)s;
+-(DBFS_Blob)slurp:(FILE *)in;
+-(int)getFile:(NSString *)fname fromDatabase:(DBFS *)dbfs to:(FILE *)out;
+-(int)putFile:(NSString *)fname fromDatabase:(DBFS *)dbfs from:(FILE *)in;
+-(int)overwriteFile:(NSString *)fname inDatabase:(DBFS *)dbfs from:(FILE *)in;
+-(int)deleteFile:(NSString *)fname fromDB:(DBFS *)dbfs;
+-(DBFS_FileList *)getFileListIn:(NSString *)dirName fromDatabase:(DBFS *)dbfs;
+-(DBFS_DirList *)getDirectoryListIn:(NSString *)dirName inDatabase:(DBFS *)dbfs;
 
 @end
--- DBInterface.m
+++ DBInterface.m
@@ -8,13 +8,8 @@
 
 #import "DBInterface.h"
 #import "IPadTableViewController.h"
-#import "mobileDrive/dbfs/dbfs.h"
 
 @implementation DBInterface
-
-//-(void)displayError:(NSString *)msg {
-//    NSLog([NSString msg]);
-//}
 
 -(char *)nsStringToCString:(NSString *)s {
     
@@ -32,16 +27,37 @@
     
 }
 
-
--(int)getFile:(NSString *)fname fromDB:(DBFS *)dbfs to:(FILE *)out {
+-(DBFS_Blob)slurp:(FILE *)in {
+    uint8_t *buf = NULL;
+    size_t size = 0, cap = 0;
     
-    char *name = [self nsStringToCString:fname];
+    cap = 4096;
+    buf = malloc(cap);
+    while (true)
+    {
+        size_t got;
+        if (size == cap)
+        {
+            cap *= 2;
+            buf = realloc(buf, cap);
+        }
+        got = fread(buf + size, 1, cap - size, in);
+        if (!got)
+            break;
+        size += got;
+    }
+    buf = realloc(buf, size);
+    return (DBFS_Blob){buf, size};
+}
+
+-(int)getFile:(NSString *)fname fromDatabase:(DBFS *)dbfs to:(FILE *)out {
     
     DBFS_Blob blob;
     if(!fname) {
         NSLog(@"fname missing");
         return 1;
     }
+    char *name = [self nsStringToCString:fname];
     if(DBFS_OKAY != dbfs_get(dbfs, (DBFS_FileName){name}, &blob)) {
         NSLog(@"Error getting file");
         dbfs_free_blob(blob);
@@ -54,6 +70,89 @@
     return 0;
 }
 
--(int)putFile:(NSString *)
+-(int)putFile:(NSString *)fname fromDatabase:(DBFS *)dbfs from:(FILE *)in {
+    DBFS_Blob blob;
+    if (!fname) {
+        NSLog(@"fname missing");
+        return 1;
+    }
+    char *name = [self nsStringToCString:fname];
+   
+    blob = [self slurp:in];
+    if(DBFS_OKAY != dbfs_put(dbfs, (DBFS_FileName){name}, blob)) {
+        NSLog(@"Error putting to Database");
+        free((uint8_t*)blob.data);
+        return 2;
+    }
+    return 0;
+}
+
+-(int)overwriteFile:(NSString *)fname inDatabase:(DBFS *)dbfs from:(FILE *)in {
+    
+    DBFS_Blob blob;
+    if(!fname) {
+        NSLog(@"Error: No name");
+        return 1;
+    }
+     char *name = [self nsStringToCString:fname];
+   
+    blob = [self slurp:in];
+    if (DBFS_OKAY != dbfs_ovr(dbfs, (DBFS_FileName){name}, blob)) {
+        NSLog(@"Error overwriting file in database");
+        free((uint8_t *)blob.data);
+        return 2;
+    }
+    
+    return 0;
+}
+
+-(int)deleteFile:(NSString *)fname fromDB:(DBFS *)dbfs {
+    
+    if (!fname) {
+        NSLog(@"Error: No name");
+        return 1;
+    }
+    char *name = [self nsStringToCString:fname];
+    if (DBFS_OKAY != dbfs_del(dbfs, (DBFS_FileName){name})) {
+        NSLog(@"Error deleting file from Database");
+        return 2;
+    }
+        
+    return 0;
+}
+
+-(DBFS_FileList *)getFileListIn:(NSString *)dirName fromDatabase:(DBFS *)dbfs {
+    
+    if (!dirName) {
+        NSLog(@"Error: No name");
+        return nil;
+    }
+    
+    char *dname = [self nsStringToCString:dirName];
+    DBFS_FileList *flist = nil;
+    if (DBFS_OKAY != dbfs_lsf(dbfs, (DBFS_DirName){dname}, flist)) {
+        NSLog(@"Error getting file list");
+        return nil;
+    }
+    
+    return flist;
+}
+
+-(DBFS_DirList *)getDirectoryListIn:(NSString *)dirName inDatabase:(DBFS *)dbfs {
+    
+    DBFS_DirList *dlist = nil;
+    
+    if (!dirName) {
+        NSLog(@"Error: No name");
+        return nil;
+    }
+    char *dName = [self nsStringToCString:dirName];
+    if (DBFS_OKAY != dbfs_lsd(dbfs, (DBFS_DirName){dName}, dlist)) {
+        NSLog(@"Error listing directories");
+        return nil;
+    }
+    
+    return dlist;
+}
 
 @end
