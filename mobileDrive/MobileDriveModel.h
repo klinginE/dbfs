b6a138abd4bb4c3fb3f72b369351b43ce78b9b5c
jesse.pitel <jessepcyt89@gmail.com>
2014-03-13 14:36:06 -0700

    added database interface to MobileDriveModel.

--- mobileDrive/MobileDriveModel.h
+++ mobileDrive/MobileDriveModel.h
@@ -7,7 +7,25 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "dbfs/DBInterface.h"
+#import "dbfs/dbfs.h"
 
 @interface MobileDriveModel : NSObject
+
+-(DBFS_Blob)slurp:(FILE *)in;
+
+// Retrieve contents of absolute path "fname" through file stream "out"
+-(void)getFile:(NSString *)fname to:(FILE *)out withSize:(int *)size;
+
+// Upload contents of file to absolute path "fname" through file stream "in"
+-(void)putFile:(NSString *)fname from:(FILE *)in withSize:(int)size;
+
+-(void)overwriteFile:(NSString *)fname from:(FILE *)in;
+
+// Remove file with absolute path "fname" from database
+-(void)deleteFile:(NSString *)fname;
+
+-(DBFS_FileList *)getFileListIn:(NSString *)dirName;
+-(DBFS_DirList *)getDirectoryListIn:(NSString *)dirName;
 
 @end
--- mobileDrive/MobileDriveModel.m
+++ mobileDrive/MobileDriveModel.m
@@ -12,12 +12,44 @@
 @implementation MobileDriveModel {
     NSDictionary *_directoryContents;
     NSArray *_directoryKeys;
+    DBInterface *dbInterface;
+    DBFS *dbfs;
 }
 
 -(NSDictionary *)getCurrentContents {
     return _directoryContents;
 }
 
+
+-(DBFS_Blob)slurp:(FILE *)in {
+    DBFS_Blob blob;
+    
+    return blob;
+}
+
+-(void)getFile:(NSString *)fname to:(FILE *)out withSize:(int *)size {
+    [dbInterface getFile:fname fromDatabase:dbfs to:out withSize:size];
+}
+
+-(void)putFile:(NSString *)fname from:(FILE *)in withSize:(int)size {
+    [dbInterface putFile:fname fromDatabase:dbfs from:in withSize:size];
+}
+
+-(void)overwriteFile:(NSString *)fname from:(FILE *)in {
+    [dbInterface overwriteFile:fname inDatabase:dbfs from:in];
+}
+
+-(void)deleteFile:(NSString *)fname {
+    [dbInterface deleteFile:fname fromDatabase:dbfs];
+}
+
+-(DBFS_FileList *)getFileListIn:(NSString *)dirName {
+    return [dbInterface getFileListIn:dirName fromDatabase:dbfs];
+}
+
+-(DBFS_DirList *)getDirectoryListIn:(NSString *)dirName {
+    return [dbInterface getDirectoryListIn:dirName inDatabase:dbfs];
+}
 
 
 @end
