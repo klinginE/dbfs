4722d1cdad975c4197be6fdca107c090030bba53
Jesse Pitel <jessepcyt89@gmail.com>
2014-04-28 19:51:34 -0700

    almost Finished model comments

--- mobileDrive/MobileDriveModel.m
+++ mobileDrive/MobileDriveModel.m
@@ -16,12 +16,19 @@
 
 }
 
+// =============================================
+// Creates an instance of the model and opens
+// the database with set name so that the same
+// database is opened every time the app starts.
+// =============================================
 -(id)init {
     self = [super init];
     if (self) {
 
         _interfaceQueue = dispatch_queue_create("interfaceThread", DISPATCH_QUEUE_SERIAL);
 
+        // All interactions with the database interface must be synchronous,
+        // so they take the following form.
         dispatch_block_t block = ^{
             self.dbInterface = [[DBInterface alloc] init];
         };
@@ -41,6 +48,10 @@
     return self;
 }
 
+// =============================================
+// Retrieves the error message corresponding to
+// a given error number.
+// =============================================
 -(NSString *)dbError:(int)err {
     
     __block NSString *r = 0;
@@ -54,6 +65,9 @@
 
 }
 
+// =============================================
+// Closes the database synchronously.
+// =============================================
 -(void)closeDatabase {
 
     dispatch_block_t block = ^{
@@ -63,6 +77,11 @@
 
 }
 
+// =============================================
+// Restarts the database.
+// Deletes all database content and creates an
+// empty one.
+// =============================================
 -(void)deleteDatabaseRecreate:(BOOL)flag {
 
     [self closeDatabase];
@@ -90,6 +109,11 @@
 
 }
 
+// =============================================
+// Writes the concent of a given file to a file
+// stream.
+// Returns an error number or OK.
+// =============================================
 -(int)getFile:(NSString *)fname to:(FILE *)out withSize:(int *)size {
     if (fname == nil) {
         return DBFS_NOT_FILENAME;
@@ -113,6 +137,10 @@
 
 }
 
+// =============================================
+// Returns an NSData object containing the data
+// from a given file.
+// =============================================
 -(NSData *)getFile_NSDATA:(NSString *)fname{
     if (fname == nil) {
         return nil;
@@ -130,6 +158,10 @@
     return result;
 }
 
+// =============================================
+// Inserts a file into the database using a file
+// stream.
+// =============================================
 -(int)putFile:(NSString *)fname from:(FILE *)in withSize:(int)size {
     if (fname == nil) {
         return DBFS_NOT_FILENAME;
@@ -151,24 +183,11 @@
     return result;
 
 }
--(int)overwriteFile_NSDATA:(NSString *)fname BLOB: (NSData*) blob {
-    if (fname == nil) {
-        return DBFS_NOT_FILENAME;
-    }
-    else if ([fname isEqualToString:@""]) {
-        return DBFS_NOT_FILENAME;
-    }
-    __block int result = DBFS_LIGHTS_ON;
-    __block NSString *fname_t = fname.copy;
-    dispatch_block_t block = ^{
-        result = [self.dbInterface overwriteFile_NSDATA:fname_t
-                                                   Blob:blob
-                                           fromDatabase:self->dbfs];
-    };
-    dispatch_sync(self.interfaceQueue, block);
-    return result;
-}
 
+// =============================================
+// Inserts a file into the database from the
+// content of an NSData object.
+// =============================================
 -(int)putFile_NSDATA:(NSString *)fname BLOB: (NSData*) blob {
     if (fname == nil || blob == nil) {
         return DBFS_NOT_FILENAME;
@@ -188,6 +207,54 @@
     return result;
 }
 
+// =============================================
+// Replaces the contents of a given file using a
+// file stream.
+// =============================================
+-(int)overwriteFile:(NSString *)fname from:(FILE *)in {
+    if (fname == nil) {
+        return DBFS_NOT_FILENAME;
+    }
+    else if ([fname isEqualToString:@""]) {
+        return DBFS_NOT_FILENAME;
+    }
+    __block int result = DBFS_LIGHTS_ON;
+    __block NSString *fname_t = fname.copy;
+    __block FILE *in_t = in;
+    dispatch_block_t block = ^{
+        result = [self.dbInterface overwriteFile:fname_t
+                                      inDatabase:self->dbfs
+                                            from:in_t];
+    };
+    dispatch_sync(self.interfaceQueue, block);
+    return result;
+}
+
+// =============================================
+// Replaces the content of a given file with the
+// content of a given NSData object.
+// =============================================
+-(int)overwriteFile_NSDATA:(NSString *)fname BLOB: (NSData*) blob {
+    if (fname == nil) {
+        return DBFS_NOT_FILENAME;
+    }
+    else if ([fname isEqualToString:@""]) {
+        return DBFS_NOT_FILENAME;
+    }
+    __block int result = DBFS_LIGHTS_ON;
+    __block NSString *fname_t = fname.copy;
+    dispatch_block_t block = ^{
+        result = [self.dbInterface overwriteFile_NSDATA:fname_t
+                                                   Blob:blob
+                                           fromDatabase:self->dbfs];
+    };
+    dispatch_sync(self.interfaceQueue, block);
+    return result;
+}
+
+// =============================================
+// Changes the pathname of a file.
+// =============================================
 -(int)renameFile:(NSString *)oldName to:(NSString *)newName {
     if (oldName == nil || newName == nil) {
         return DBFS_NOT_FILENAME;
@@ -209,6 +276,9 @@
 
 }
 
+// =============================================
+// Changes the pathname of a file.
+// =============================================
 -(int)moveFile:(NSString *)oldName to:(NSString *)newName {
     if (oldName == nil || newName == nil) {
         return DBFS_NOT_FILENAME;
@@ -228,26 +298,9 @@
     return result;
 }
 
--(int)overwriteFile:(NSString *)fname from:(FILE *)in {
-    if (fname == nil) {
-        return DBFS_NOT_FILENAME;
-    }
-    else if ([fname isEqualToString:@""]) {
-        return DBFS_NOT_FILENAME;
-    }
-    __block int result = DBFS_LIGHTS_ON;
-    __block NSString *fname_t = fname.copy;
-    __block FILE *in_t = in;
-    dispatch_block_t block = ^{
-        result = [self.dbInterface overwriteFile:fname_t
-                                      inDatabase:self->dbfs
-                                            from:in_t];
-    };
-    dispatch_sync(self.interfaceQueue, block);
-    return result;
-
-}
-
+// =============================================
+// Removes a given file from the database.
+// =============================================
 -(int)deleteFile:(NSString *)fname {
     if (fname == nil) {
         return DBFS_NOT_FILENAME;
@@ -267,6 +320,10 @@
 
 }
 
+// =============================================
+// Creates an empty directory with the given
+// full path name.
+// =============================================
 -(int)createDirectory:(NSString *)dirName {
     if (dirName == nil) {
         return DBFS_NOT_DIRNAME;
@@ -286,6 +343,10 @@
 
 }
 
+// ==============================================
+// Removes the given directory from the database.
+// Also removes all contents and subdirectories.
+// ==============================================
 -(int)deleteDirectory:(NSString *)dirName {
     if (dirName == nil) {
         return DBFS_NOT_DIRNAME;
@@ -303,6 +364,10 @@
     return result;
 }
 
+// =============================================
+// Changes the path of a given directory.
+// Also moves all content.
+// =============================================
 -(int)moveDirectory:(NSString *)dirName to:(NSString *)destName {
     if (dirName == nil || destName == nil) {
         return DBFS_NOT_FILENAME;
@@ -323,6 +388,9 @@
 
 }
 
+// =============================================
+// Changes the path name of a given directory.
+// =============================================
 -(int)renameDirectory:(NSString *)dirName to:(NSString *)newName {
     if (dirName == nil || newName == nil) {
         return DBFS_NOT_FILENAME;
@@ -342,6 +410,10 @@
     return result;
 }
 
+// =============================================
+// Creates an alphabetized array of the files
+// in the given directory.
+// =============================================
 -(NSArray *)getFileArrayIn:(NSString *)dirName {
     __block DBFS_FileList fileList;
     __block NSString *dirName_t = dirName.copy;
@@ -354,6 +426,7 @@
     NSMutableDictionary *fileDict = [[NSMutableDictionary alloc] init];
     NSMutableArray *keys = [[NSMutableArray alloc] init];
     
+    // First create a dictionary of the files with the names as keys
     for (int i = 0; i < fileList.count; i++) {
         NSString *name = [[NSString alloc] initWithUTF8String:((fileList.files)+i)->name];
         NSDictionary *d = [[NSDictionary alloc] initWithObjectsAndKeys:name, @"name",
@@ -366,14 +439,19 @@
         [fileDict setObject:d forKey:[keys objectAtIndex:i]];
     }
     
-    /* Alphabetize the file list */
+    // Then alphabetize the keys
     NSArray *alphabeticalKeys = [[fileDict allKeys] sortedArrayUsingSelector:@selector(compare:)];
+    // And create an array of just the files according to the order of the keys
     NSArray *objects = [fileDict objectsForKeys:alphabeticalKeys
                                  notFoundMarker:[NSNull null]];
     
     return objects;
 }
 
+// ================================================
+// Creates an alphabetized array of the directories
+// in the given directory.
+// ================================================
 -(NSArray *)getDirectoryArrayIn:(NSString *)dirName {
     __block DBFS_DirList dirList;
     NSString *dirName_t = dirName.copy;
@@ -385,7 +463,8 @@
 
     NSMutableDictionary *dirDict = [[NSMutableDictionary alloc] init];
     NSMutableArray *keys = [[NSMutableArray alloc] init];
-    
+
+    // First create a dictionary of the directories with their names as keys
     for (int i = 0; i < dirList.count; i++) {
         NSString *name = [[NSString alloc] initWithUTF8String:((dirList.dirs)+i)->name];
         NSDictionary *d = [[NSDictionary alloc] initWithObjectsAndKeys:name, @"name",
@@ -397,14 +476,20 @@
         [dirDict setObject:d forKey:[keys objectAtIndex:i]];
     }
     
-    /* Alphabetize the directory list */
+    // Then alphabetize the keys
     NSArray *alphabeticalKeys = [[dirDict allKeys] sortedArrayUsingSelector:@selector(compare:)];
+    // And create an array of the directories according to the keys
     NSArray *objects = [dirDict objectsForKeys:alphabeticalKeys
                                 notFoundMarker:[NSNull null]];
 
     return objects;
 }
 
+// ===============================================
+// Creates an array of the contents of the given
+// directory. Starts with alphabetical directories
+// followed by alphabetical files.
+// ===============================================
 -(NSArray *)getContentsArrayIn:(NSString *)dirName {
     NSMutableArray *contentArray = [[NSMutableArray alloc] initWithArray:[self getDirectoryArrayIn:dirName]];
     NSArray *tempArray = [self getFileArrayIn:dirName];
@@ -412,7 +497,10 @@
     return contentArray;
 }
 
-
+// ===============================================
+// Creates an alphabetical dictionary of the files
+// in a given directory. File names as keys.
+// ===============================================
 -(NSDictionary *)getFileListIn:(NSString *)dirName {
     __block DBFS_FileList fileList;
     __block NSString *dirName_t = dirName.copy;
@@ -425,6 +513,7 @@
     NSMutableDictionary *fileDict = [[NSMutableDictionary alloc] init];
     NSMutableArray *keys = [[NSMutableArray alloc] init];
 
+    // First create a dictionary of the files
     for (int i = 0; i < fileList.count; i++) {
         NSString *name = [[NSString alloc] initWithUTF8String:((fileList.files)+i)->name];
         NSDictionary *d = [[NSDictionary alloc] initWithObjectsAndKeys:name, @"name",
@@ -436,17 +525,22 @@
         [fileDict setObject:d forKey:[keys objectAtIndex:i]];
     }
     
-    /* Alphabetize the file list */
+    // Then alphabetize the keys
     NSArray *alphabeticalKeys = [[fileDict allKeys] sortedArrayUsingSelector:@selector(compare:)];
+    // Then create an array of the files based on the ordered keys
     NSArray *objects = [fileDict objectsForKeys:alphabeticalKeys
                                  notFoundMarker:[NSNull null]];
-    
+    // Finally, recreate the dictionary based on the alphabetical array.
     NSDictionary *finalDict = [[NSDictionary alloc] initWithObjects:objects
                                                             forKeys:alphabeticalKeys];
     
     return finalDict;
 }
 
+// =============================================
+// Creates an alphabetical dictionary of the
+// directories in a given directory.
+// =============================================
 -(NSDictionary *)getDirectoryListIn:(NSString *)dirName {
     __block DBFS_DirList dirList;
     __block NSString *dirName_t = dirName.copy;
@@ -459,6 +553,7 @@
     NSMutableDictionary *dirDict = [[NSMutableDictionary alloc] init];
     NSMutableArray *keys = [[NSMutableArray alloc] init];
  
+	// First create a dictionary of the directories
     for (int i = 0; i < dirList.count; i++) {
         NSString *name = [[NSString alloc] initWithUTF8String:((dirList.dirs)+i)->name];
         NSDictionary *d = [[NSDictionary alloc] initWithObjectsAndKeys:name, @"name",
@@ -471,17 +566,22 @@
         [dirDict setObject:d forKey:[keys objectAtIndex:i]];
     }
     
-    /* Alphabetize the directory list */
-    
+    // Then alphabetize the keys 
     NSArray *alphabeticalKeys = [[dirDict allKeys] sortedArrayUsingSelector:@selector(compare:)];
+    // And create an array based on the ordered keys
     NSArray *objects = [dirDict objectsForKeys:alphabeticalKeys
                                 notFoundMarker:[NSNull null]];
-
+	// Finally, recreate the dictionary from the ordered array
     NSDictionary *finalDict = [[NSDictionary alloc] initWithObjects:objects
                                                             forKeys:alphabeticalKeys];
     return finalDict;
 }
 
+// =============================================
+// Creates an alphabetical dictionary with all
+// the contents of the given directory. Starts 
+// with directories followed by files.
+// =============================================
 -(NSDictionary *)getContentsIn:(NSString *)dirName {
     NSMutableDictionary *contentDict = [[NSMutableDictionary alloc] initWithDictionary:[self getDirectoryListIn:dirName]];
     NSDictionary *tempDict = [self getFileListIn:dirName];
